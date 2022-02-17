87bcfc75a47bd26644f97b2e79fbc7629213c1df
zeiros20 <70537247+zeiros20@users.noreply.github.com>
2022-02-17 21:45:13 +0200

    Delete operation.py

--- operation.py
+++
@@ -1,9 +0,0 @@
-from sys import path
-path.append(r"/home/zeiros/MPC_general_model/Macenum_Model")
-from Macenum_Model import MACE
-
-mace = MACE(0.1,0.2,0.1,0,0,0,15,15,0,0.2,20)
-mace.set_weights(9,7,1,0.05,0.05,0.05,0.05)
-mace.init_variables()
-mace.operate()
-
